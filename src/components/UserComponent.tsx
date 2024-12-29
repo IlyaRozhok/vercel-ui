ba00b7dd33ae73535e1f15b3ea0e9d709a47bdf2
IlyaRozhok <ilya.rozhok@igamemedia.com>
2024-12-30 00:48:56 +0200

    Google auth, one step onboarding

---
+++ src/components/UserComponent.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from "react";
+import Home from "@/components/Home/Home";
+
+const UserComponent = () => {
+
+    return (
+        <div>
+            <Home/>
+        </div>
+    );
+};
+
+export default UserComponent;
